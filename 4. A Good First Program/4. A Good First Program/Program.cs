46e7c6587ed36a789d973a4c8231cc3de58cdf09
Danielle Murphy <Danielle0876@gmail.com>
2020-04-10 21:36:24 +0100

    Programming by doing exercises

---
+++ 4. A Good First Program/4. A Good First Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4._A_Good_First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Again");
+            Console.WriteLine("I like typing this.");
+            Console.WriteLine("This is fun.");
+            Console.WriteLine("Yay! Printing.");
+            Console.WriteLine("I'd much rather you 'not'.");
+            Console.WriteLine("I \"said\" do not touch this.");
+        }
+    }
+}
---
+++ 5. Comments and Slashes/5. Comments and Slashes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5._Comments_and_Slashes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A comment, this is so you can read your program later.
+            // Anything after the // is ignored.
+
+            Console.WriteLine("I could have code like this."); // and the comment after is ignored.
+
+            /* You can also use a comment to "disable" or comment out a piece of code:
+             Console.WriteLine("This won't run."); */
+
+            Console.WriteLine("This will run.");
+        }
+    }
+}
