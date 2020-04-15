e7a1513c206f52304f21982e3c5c1ca95166d408
Danielle Murphy <Danielle0876@gmail.com>
2020-04-15 20:51:34 +0100

    next exercises

---
+++ 10. More Varaibles and Printing/10. More Varaibles and Printing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10._More_Varaibles_and_Printing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName, myEyes, myTeeth, myHair;
+            int myAge, myHeight, myWeight;
+
+            myName = "Zed";
+            myAge = 35;     
+            myHeight = 74;  
+            myWeight = 140;  
+            myEyes = "Blue";
+            myTeeth = "White";
+            myHair = "Brown";
+
+            Console.WriteLine("Let's talk about " + myName + ".");
+            Console.WriteLine("He's " + myHeight + " inches tall.");
+            Console.WriteLine("He's " + myWeight + " pounds heavy.");
+            Console.WriteLine("Actually, that's not too heavy.");
+            Console.WriteLine("He's got " + myEyes + " eyes and " + myHair + " hair.");
+            Console.WriteLine("His teeth are usually " + myTeeth + " depending on the coffee.");
+
+
+            Console.WriteLine("If I add " + myAge + ", " + myHeight + ", and " + myWeight);
+            Console.WriteLine(" I get " + (myAge + myHeight + myWeight) + ".");
+        }
+    }
+}
---
+++ 11. Using Variables/11. Using Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11._Using_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int room;
+            double e;
+            string computer_Science;
+
+            room = 113;
+            e = 2.71828;
+            computer_Science = "Computer Science";
+
+            Console.WriteLine("This is room #" + room);
+            Console.WriteLine("e is close to " + e);
+            Console.WriteLine("I am learning a bit about " +computer_Science);
+
+
+        }
+    }
+}
