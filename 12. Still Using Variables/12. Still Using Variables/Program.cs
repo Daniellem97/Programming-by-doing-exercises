5117eb8d9be5cb95bb514c814ee0e17e0970a468
Danielle Murphy <Danielle0876@gmail.com>
2020-04-16 12:53:03 +0100

    exercises 12 & 13 completed

---
+++ 12. Still Using Variables/12. Still Using Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12._Still_Using_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int year;
+
+            name = "Danielle";
+            year = 2021;
+
+            Console.WriteLine("my name is {0} and ill graduate in {1}", name, year);
+
+        }
+    }
+}
---
+++ 13. Your Schedule/13. Your Schedule/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _13._Your_Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string course1, course2, course3, course4, course5, course6, teacher1, teacher2, teacher3, teacher4, teacher5, teacher6;
+
+            course1 = "maths";
+            course2 = "english";
+            course3 = "irish";
+            course4 = "geography";
+            course5 = "histroy";
+            course6 = "french";
+
+            teacher1 = "Mr Murphy";
+            teacher2 = "Ms Burke";
+            teacher3 = "Mr Maguire";
+            teacher4 = "Ms Byrne";
+            teacher5 = "Mr Gargan";
+            teacher6 = "Mr Wallace";
+
+
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("| " + course1 + " | " + teacher1 + " |");
+            Console.WriteLine("| " + course2 + " | " + teacher2 + " |");
+            Console.WriteLine("| " + course3 + " | " + teacher3 + " |");
+            Console.WriteLine("| " + course4 + " | " + teacher4 + " |");
+            Console.WriteLine("| " + course5 + " | " + teacher5 + " |");
+            Console.WriteLine("| " + course6 + " | " + teacher6 + " |");
+            Console.WriteLine("+------------------------+");
+        }
+        
+    }
+}
