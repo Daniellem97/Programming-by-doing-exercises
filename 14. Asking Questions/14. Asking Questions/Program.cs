73ee970fc2560be7a78d701451453021bacbd20b
Danielle Murphy <Danielle0876@gmail.com>
2020-04-18 22:14:05 +0100

    exercises 14 and 15 completed

---
+++ 14. Asking Questions/14. Asking Questions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14._Asking_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int age;
+			String height;
+			double weight;
+
+			Console.WriteLine("How old are you? ");
+			age = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("How tall are you? ");
+			height = Console.ReadLine();
+
+			Console.WriteLine("How much do you weigh in kgs? ");
+			weight = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("So you're " + age + " years old, " + height + " tall and " + weight + "kgs heavy.");
+		}
+    }
+}
---
+++ 15. The forgetful machine/15. The forgetful machine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _15._The_forgetful_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a word!");
+            Console.ReadLine();
+            Console.WriteLine("Give me a second word!");
+            Console.ReadLine();
+            Console.WriteLine("Great, now whats your favourite number?");
+            Console.ReadLine();
+            Console.WriteLine("And your second-favourite number...");
+            Console.ReadLine();
+            Console.WriteLine("Whew! wasnt that fun?");
+        }
+
+    }
+}
