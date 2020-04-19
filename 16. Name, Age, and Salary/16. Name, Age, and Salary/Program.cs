439e1c801938e7fc8721450305635d57a5a34ef9
Danielle Murphy <Danielle0876@gmail.com>
2020-04-19 16:53:34 +0100

    exercises 16 and 17 completed

---
+++ 16. Name, Age, and Salary/16. Name, Age, and Salary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _16._Name__Age__and_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            double salary;
+            Console.WriteLine("Hello , what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi,{0} ! How old are you?" , name);
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("so you're {0}, eh? Thats not old at all! How much do you make, {1}" , age, name);
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("so {0} , you're {1} and you make {2}, got it!" , name, age, salary);
+
+
+        }
+    }
+}
---
+++ 17. More User Input/17. More User Input/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _17._More_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName, userLogin;
+            int age;
+            double studentID, GPA;
+
+            Console.WriteLine("Hello whats your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("whats your last name?");
+            lastName = Console.ReadLine();
+            Console.WriteLine("what age are you?");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("what is your student ID?");
+            studentID = double.Parse(Console.ReadLine());
+            Console.WriteLine("what is your user login?");
+            userLogin = Console.ReadLine();
+            Console.WriteLine("what is your gpa 0.0-4.0?");
+            GPA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Login: {0}" , userLogin);
+            Console.WriteLine("ID: {0}", studentID);
+            Console.WriteLine("Name: {0}, {1}", lastName, firstName);
+            Console.WriteLine("GPA: {0}", GPA);
+            Console.WriteLine("Age: {0}", age);
+        }
+    }
+}
