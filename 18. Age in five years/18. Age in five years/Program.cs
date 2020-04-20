92713aca59dde480e290b416e48faeef29d40410
Danielle Murphy <Danielle0876@gmail.com>
2020-04-20 14:49:04 +0100

    exercise 18 complete

---
+++ 18. Age in five years/18. Age in five years/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _18._Age_in_five_years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age, beforeage, afterage;
+
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi {0}! How old are you?",name);
+            age = int.Parse(Console.ReadLine());
+            beforeage = (age - 5);
+            afterage = (age + 5);
+            Console.WriteLine("did you know that in five years you will be {0} years old? and five years ago you were {1}! imagine that!", afterage, beforeage);
+
+        }
+    }
+}
