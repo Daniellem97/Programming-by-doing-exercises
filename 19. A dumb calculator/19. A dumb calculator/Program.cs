c65e9815f7826f332cfa1ccfcdd55eabf83cfe94
Danielle Murphy <Danielle0876@gmail.com>
2020-04-21 22:38:21 +0100

    exercise 19 complete

---
+++ 19. A dumb calculator/19. A dumb calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _19._A_dumb_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3, answer;
+
+            Console.WriteLine("what is your first number?");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("what is your second number?");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("what is your third number?");
+            num3 = double.Parse(Console.ReadLine());
+
+            answer = ((num1 + num2 + num3) / 2);
+            Console.WriteLine(answer);
+        }
+    }
+}
