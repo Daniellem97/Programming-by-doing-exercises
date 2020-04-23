f5e0602df9f9f3321869738407c755162325fa8a
Danielle Murphy <Danielle0876@gmail.com>
2020-04-23 20:16:49 +0100

    exerciese 20 and 21 complete

---
+++ 20. BMI calculator/20. BMI calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20._BMI_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height, kg, bmi, heightSquared;
+
+            Console.WriteLine("your height in m?");
+            height = double.Parse(Console.ReadLine());
+            Console.WriteLine("your weight in kg?");
+            kg = double.Parse(Console.ReadLine());
+            heightSquared = Math.Pow(height, 2);
+            bmi = kg / heightSquared;
+            Console.WriteLine("your bmi is {0}", bmi);
+        }
+    }
+}
---
+++ 21. What if statement/What if statement 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace What_if_statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+			{
+				int people = 20;
+				int cats = 30;
+				int dogs = 15;
+
+				if (people < cats)
+				{
+					Console.WriteLine("Too many cats!  The world is doomed!");
+				}
+
+				if (people > cats)
+				{
+					Console.WriteLine("Not many cats!  The world is saved!");
+				}
+
+				if (people < dogs)
+				{
+					Console.WriteLine("The world is drooled on!");
+				}
+
+				if (people > dogs)
+				{
+					Console.WriteLine( "The world is dry!");
+				}
+
+				if (people >= dogs)
+				{
+					Console.WriteLine("People are greater than or equal to dogs.");
+				}
+
+				if (people <= dogs)
+				{
+					Console.WriteLine("People are less than or equal to dogs.");
+				}
+
+				if (people == dogs)
+				{
+					Console.WriteLine("People are dogs.");
+				}
+			}
+    }
+}
