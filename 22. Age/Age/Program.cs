4418f2a0c8bb33e22932a736fa1b269e3d5b2837
Danielle Murphy <Danielle0876@gmail.com>
2020-04-27 20:36:12 +0100

    exercise 22 complete

---
+++ 22. Age/Age/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("how year were you born?");
+            int DOB = int.Parse(Console.ReadLine());
+            int age = (2020 - DOB);
+            Console.WriteLine("ok {0}, you are {1} and", name, age);
+            if (age < 16)
+                Console.WriteLine("you can't drive.");
+            if (age < 18)
+                Console.WriteLine("you can't drink.");
+            if (age >= 18)
+                Console.WriteLine("you can do anything that's legal.");
+        }
+    }
+}
